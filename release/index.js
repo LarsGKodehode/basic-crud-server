0b84d9652804f7af6eb3d398a1b430547f935e7e
LarsGKodehode <lars.gunnar.kodehodet@gmail.com>
2022-09-05 14:27:02 +0200

    removed transpiled output from repo

--- release/index.js
+++ release/index.js
@@ -1,1 +1,12 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require("fs");
+fs.readFile("./src/data/simpleText", (error, data) => {
+    if (error)
+        throw error;
+    console.log(data);
+});
+process.on("uncaughtException", (error, origin) => {
+    fs.writeSync(process.stderr.fd, `Cought exception: ${error}\n` +
+        `Exception origin: ${origin}\n`);
+});
