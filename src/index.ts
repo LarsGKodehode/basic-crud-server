da14921a62aeb65c8c995c70deed0de8816da5b9
Lars G <104063134+LarsGKodehode@users.noreply.github.com>
2022-09-06 13:31:22 +0200

    finishing up task (#1)

---
+++ src/index.ts
@@ -0,0 +1,71 @@
+// Node modules
+import fs = require("fs");
+import path = require("path");
+
+
+// Paths
+const PATH_DATA = "./data/";
+
+
+/**
+ * Reads a text file
+ */
+function ReadTextFile(filePath: string, fileName: string) {
+  const encoding = "utf8";
+  const fullPath = path.join(__dirname, filePath, fileName);
+
+  fs.readFile(fullPath, encoding,
+    (error, data) => {
+      if (error) throw error;
+      console.log(data.toString());
+  });
+};
+
+/**
+ * Write a string to existing file
+ */
+function WriteToTextFile(filePath: string, fileName: string, text: string) {
+  const fullPath = path.join(__dirname, filePath, fileName);
+
+  fs.writeFile(fullPath, text,
+    (error) => {
+      if (error) throw error;
+      console.log("Writing done");
+  });
+};
+
+/**
+ * Append, Create file
+ */
+function AppendTextToFile(filePath: string, fileName: string, text: string) {
+  const fullPath = path.join(__dirname, filePath, fileName);
+
+  fs.appendFile(fullPath, text,
+    (error) => {
+      if (error) throw error;
+      console.log("Appending done");
+  });
+};
+
+
+// Samples
+ReadTextFile(PATH_DATA, "simpleText.txt");
+
+const greeting = "Hi there and hello";
+WriteToTextFile(PATH_DATA, "greeting.txt", greeting);
+
+const appendText = "This text was appended";
+AppendTextToFile(PATH_DATA, "greeting.txt", appendText);
+
+const newText = "This file was created from scratch";
+AppendTextToFile(PATH_DATA, "new.txt", newText);
+
+
+// Error handling
+process.on("uncaughtException", (error, origin) => {
+  fs.writeSync(
+    process.stderr.fd,
+    `Cought exception: ${error}\n` +
+    `Exception origin: ${origin}\n`
+  );
+});
